13dbf458d3425f45749730ff8171b2cbca10006c
Noam2Stein <noamsteindev@gmail.com>
2025-03-06 09:15:30 +0200

    parser macros upgrade

--- compiler/oath_parser/derive_macro_utils/src/impl_parse.rs
+++ compiler/oath_parser/derive_macro_utils/src/impl_parse.rs
@@ -1,8 +1,8 @@
 use proc_macro2::TokenStream;
-use quote::quote;
-use syn::{DataEnum, DataStruct, Fields, spanned::Spanned};
+use quote::{quote, quote_spanned};
+use syn::{DataEnum, DataStruct, Error, Field, Fields, parse_quote_spanned, spanned::Spanned};
 
-use crate::{impl_parser_trait, peek_fields};
+use crate::impl_parser_trait;
 
 pub fn impl_parse(input: TokenStream) -> TokenStream {
     impl_parser_trait(
@@ -23,48 +23,10 @@ pub fn impl_parse(input: TokenStream) -> TokenStream {
     )
 }
 
-pub fn parse_fields(path: TokenStream, fields: Fields) -> TokenStream {
+pub fn parse_fields(path: TokenStream, fields: &Fields) -> TokenStream {
     match fields {
         Fields::Named(fields) => {
-            let fields = fields.named.into_iter().map(|field| {
-                let ident = field.ident.unwrap();
-                let ty = field.ty;
-
-                let try_parse_attr = field
-                    .attrs
-                    .iter()
-                    .find(|attr| attr.path().is_ident("try_parse"));
-
-                let parse_attr = field
-                    .attrs
-                    .iter()
-                    .find(|attr| attr.path().is_ident("parse"));
-
-                let parse_expr = if let Some(parse_attr) = parse_attr {
-                    let parse_fn = &parse_attr.meta.require_list().unwrap().tokens;
-                    quote! {
-                        (#parse_fn)()
-                    }
-                } else if try_parse_attr.is_some() {
-                    quote! {
-                        <#ty as ::oath_parser::TryParse>::try_parse(parser, context)?
-                    }
-                } else {
-                    quote! {
-                        <#ty as ::oath_parser::Parse>::parse(parser, context)
-                    }
-                };
-
-                if try_parse_attr.is_some() {
-                    quote! {
-                        #ident: #parse_expr?
-                    }
-                } else {
-                    quote! {
-                        #ident: #parse_expr
-                    }
-                }
-            });
+            let fields = fields.named.iter().map(parse_field);
 
             quote! {
                 #path {
@@ -76,17 +38,119 @@ pub fn parse_fields(path: TokenStream, fields: Fields) -> TokenStream {
             #path
         },
         Fields::Unnamed(fields) => {
-            let field_types = fields.unnamed.iter().map(|field| &field.ty);
+            let fields = fields.unnamed.iter().map(parse_field);
 
             quote! {
-                #path(#(<#field_types as oath_parser::Parse>::parse(parser, context)), *)
+                #path(#(#fields), *)
             }
         }
     }
 }
 
+pub fn parse_fields_option(path: TokenStream, fields: &Fields) -> TokenStream {
+    let (first_field, other_fields) = {
+        let mut other_fields = fields.iter();
+        let first_field = if let Some(first_field) = other_fields.next() {
+            first_field
+        } else {
+            return Error::new(fields.span(), "expected at least one field").to_compile_error();
+        };
+        (first_field, other_fields)
+    };
+
+    let first_field_parse_expr = {
+        let first_field_ty = &first_field.ty;
+        parse_field(&Field {
+            attrs: first_field.attrs.clone(),
+            colon_token: None,
+            vis: first_field.vis.clone(),
+            ident: None,
+            mutability: first_field.mutability.clone(),
+            ty: parse_quote_spanned! { first_field.ty.span() => Option<#first_field_ty> },
+        })
+    };
+
+    let parse_expr = match fields {
+        Fields::Unit => unreachable!(),
+        Fields::Named(_) => {
+            let parse_other_fields = other_fields.map(parse_field);
+
+            let first_field_ident = &first_field.ident;
+            quote! {
+                #path {
+                    #first_field_ident: first_field,
+                    #(#parse_other_fields,)*
+                }
+            }
+        }
+        Fields::Unnamed(_) => {
+            let parse_other_fields = other_fields.map(parse_field);
+            quote! {
+                #path(first_field, #(#parse_other_fields), *)
+            }
+        }
+    };
+
+    quote! {
+        if let Some(first_field) = #first_field_parse_expr {
+            Some({
+                #parse_expr
+            })
+        } else {
+            None
+        }
+    }
+}
+
+fn parse_field(field: &Field) -> TokenStream {
+    let ident_prefix = field.ident.as_ref().map(|ident| quote! { #ident: });
+    let ty = &field.ty;
+
+    let try_parse_attr = field
+        .attrs
+        .iter()
+        .find(|attr| attr.path().is_ident("try_parse"));
+
+    let parse_attr = field
+        .attrs
+        .iter()
+        .find(|attr| attr.path().is_ident("parse"));
+
+    let parse_expr = if let Some(parse_attr) = parse_attr {
+        let parse_fn = &parse_attr.meta.require_list().unwrap().tokens;
+        let try_parse_error = try_parse_attr.map(|try_parse_attr| {
+            Error::new(
+                try_parse_attr.span(),
+                "`try_parse` is not allowed with `parse`",
+            )
+            .to_compile_error()
+        });
+
+        quote! {
+            {
+                { #try_parse_error }
+                (#parse_fn)()
+            }
+        }
+    } else if try_parse_attr.is_some() {
+        quote_spanned! {
+            ty.span() =>
+            <#ty as ::oath_parser::TryParse>::try_parse(parser, context)?
+        }
+    } else {
+        quote_spanned! {
+            ty.span() =>
+            <#ty as ::oath_parser::Parse>::parse(parser, context)
+        }
+    };
+
+    quote! {
+        #ident_prefix #parse_expr
+    }
+}
+
 fn parse_struct(data: DataStruct) -> TokenStream {
-    parse_fields(quote! { Self }, data.fields)
+    parse_fields(quote! { Self }, &data.fields)
 }
 
 pub fn parse_enum(data: DataEnum) -> TokenStream {
@@ -119,23 +183,19 @@ pub fn parse_enum(data: DataEnum) -> TokenStream {
             None
         };
 
-        let peek_variant = peek_fields(variant.fields.clone(), &variant.attrs, variant.span());
         let variant_ident = &variant.ident;
-        let parse_variant = parse_fields(quote! { Self::#variant_ident }, variant.fields.clone());
+        let parse_variant = parse_fields_option(quote! { Self::#variant_ident }, &variant.fields);
 
         quote! {
-            #option_else if #peek_variant {
-                #parse_variant
+            #option_else if let Some(variant) = (#parse_variant) {
+                variant
             }
         }
     });
 
     let fallback = if let Some(fallback_variant) = fallback_variant {
         let variant_ident = &fallback_variant.ident;
-        parse_fields(
-            quote! { Self::#variant_ident },
-            fallback_variant.fields.clone(),
-        )
+        parse_fields(quote! { Self::#variant_ident }, &fallback_variant.fields)
     } else {
         quote! {
             context.push_error(::oath_context::Error::new(format!("expected {}", <Self as ::oath_parser::Desc>::desc()), parser.next_span()));
